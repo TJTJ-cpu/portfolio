245562a48aed4a2c45b0b0199b6bf15dda5a17d3
Tungjai Mady <117895770+TJTJ-cpu@users.noreply.github.com>
2026-04-01 13:23:44 +0200

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
+import Hero from "@/components/sections/Hero";
+
 
 export default function Home() {
   return (
     <main>
       <Navbar/>
-        <h1>
-          TJ's Portfolio
-        </h1>
-        <Footer />
+      <Hero />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <section id="hero" className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">TJ Mady</h1>
+      <p className="text-xl text-gray-400 mt-4">Software Develope
+      </p>
+      <p className="text-xl text-gray-400 mt-4">Praise the Lord</p>
+    </section>
+  );
+}
