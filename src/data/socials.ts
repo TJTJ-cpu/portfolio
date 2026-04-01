e1c3fc5c029130855b6b4e0cbb9d2d817d3f6f9c
Tungjai Mady <117895770+TJTJ-cpu@users.noreply.github.com>
2026-04-01 14:52:46 +0200

    fix: email

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -3,5 +3,5 @@ import { SocialLink } from "@/types";
 export const socials: SocialLink[] = [
   { platform: "GitHub", url: "https://github.com/TJTJ-cpu", icon: "FolderGit2" },
   { platform: "LinkedIn", url: "https://linkedin.com/in/yourname", icon: "Link" },
-  { platform: "Email", url: "tjeurope2002@gmail.com", icon: "Mail" },
+  { platform: "Email", url: "mailto:tjeurope2002@gmail.com", icon: "Mail" },
 ];
