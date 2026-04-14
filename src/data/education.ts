2f1b117a5f471633f2d41b5b1c4dc155bc8a28cc
Tungjai Mady <117895770+TJTJ-cpu@users.noreply.github.com>
2026-04-14 11:51:30 +0200

    fix: Deduplicate education and adjust socials email

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,6 +2,15 @@ import { Education } from "@/types";
 
 export const education: Education[] = [
   {
+    school: "Luleå University of Technology",
+    degree: "Degree of Bachelor of Science in Engineering",
+    period: "2022 - 2025",
+    location: "Skellefteå, Sweden",
+    highlights: ["Computer Engineering - Computer Game Programming", "Final Thesis - Pinpointing Virtual Object Locations in Real World"],
+    logo: "/images/education/ltu.jpg",
+    url: "https://www.ltu.se/"
+  },
+  {
     school: "Vientiane Pattana School International",
     degree: "High School Diploma",
     period: "2007 - 2020",
@@ -11,14 +20,5 @@ export const education: Education[] = [
     ],
     logo: "/images/education/vpsi.jpg",
     url: "https://vpslao.com/"
-  },
-  {
-    school: "Luleå University of Technology",
-    degree: "Degree of Bachelor of Science in Engineering",
-    period: "2022 - 2025",
-    location: "Skellefteå, Sweden",
-    highlights: ["Computer Engineering - Computer Game Programming", "Final Thesis - Pinpointing Virtual Object Locations in Real World"],
-    logo: "/images/education/ltu.jpg",
-    url: "https://www.ltu.se/"
-  },
+  }
 ];
